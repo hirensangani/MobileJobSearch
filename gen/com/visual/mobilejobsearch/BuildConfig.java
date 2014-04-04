cbfaeaa9fe2e62979ce48432a26334d29a24802f
hiren.sangani.gec@gmail.com <hiren.sangani.gec@gmail.com>
2014-04-04 18:33:08 +0200

    first commit ever

---
+++ gen/com/visual/mobilejobsearch/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.visual.mobilejobsearch;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ListViewMainOfScreen.java
@@ -0,0 +1,12 @@
+import android.app.ListActivity;
+import android.support.v4.widget.SimpleCursorAdapter;
+
+
+public class ListViewMainOfScreen extends ListActivity{
+
+	SimpleCursorAdapter mAdapter;
+	
+	
+	
+	
+}
